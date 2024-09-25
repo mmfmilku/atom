28ce3dd96d0704070469c457c4d1960862c30c24
mmfmilku <crino1cirno@gmail.com>
2024-09-26 00:16:44 +0800

    feature:添加测试

--- atom-agent/src/main/resources/test/PreMainTest.java
+++ atom-agent/src/main/resources/test/PreMainTest.java
@@ -2,6 +2,8 @@ package org.mmfmilku.atom.agent;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class PreMainTest {
 
     private int getNum() {
@@ -12,6 +14,8 @@ public class PreMainTest {
         return 3;
     }
 
-
+    private int getSum(int a, int b) {
+        return BigDecimal.valueOf(a).multiply(new BigDecimal(b)).intValue();
+    }
 
 }
--- atom-agent/src/test/java/org/mmfmilku/atom/agent/PreMainTest.java
+++ atom-agent/src/test/java/org/mmfmilku/atom/agent/PreMainTest.java
@@ -1,6 +1,9 @@
 package org.mmfmilku.atom.agent;
 
 import org.junit.Test;
+
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 public class PreMainTest {
@@ -9,14 +12,22 @@ public class PreMainTest {
         return 2;
     }
 
+    private int getSum(long a, long b) {
+        return BigDecimal.valueOf(a).add(new BigDecimal(b)).intValue();
+    }
+
     /**
      * 启动参数添加 -javaagent:F:\dev\project\atom\atom-agent\atom-agent-core\target\atom-agent-core-jar-with-dependencies.jar=base-path=F:\dev\project\atom\atom-agent\src\main\resources\test;k2=v2
      * */
     @Test
     public void testCase1() {
         int num = getNum();
-        System.out.println(num);
+        System.out.println("num=" + num);
         assertEquals(3, num);
+
+        int sum = getSum(3, 9);
+        System.out.println("sum=" + sum);
+        assertEquals(sum, 27);
     }
 
 }
