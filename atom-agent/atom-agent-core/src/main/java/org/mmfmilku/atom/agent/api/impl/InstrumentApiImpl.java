81f862aa8f832e03fb8206f61727872dddae6600
mmfmilku <crino1cirno@gmail.com>
2024-10-10 20:20:29 +0800

    feature:添加frpc服务

---
+++ atom-agent/atom-agent-core/src/main/java/org/mmfmilku/atom/agent/api/impl/InstrumentApiImpl.java
@@ -0,0 +1,80 @@
+package org.mmfmilku.atom.agent.api.impl;
+
+import javassist.CannotCompileException;
+import javassist.NotFoundException;
+import org.mmfmilku.atom.agent.instrument.InstrumentationContext;
+import org.mmfmilku.atom.agent.util.ByteCodeUtils;
+import org.mmfmilku.atom.api.InstrumentApi;
+import org.mmfmilku.atom.transport.frpc.FRPCService;
+
+import java.io.IOException;
+import java.lang.instrument.UnmodifiableClassException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * InstrumentApiImpl
+ *
+ * @author chenxp
+ * @date 2024/10/10:10:27
+ */
+@FRPCService
+public class InstrumentApiImpl implements InstrumentApi {
+
+    @Override
+    @FRPCService
+    public List<String> listClassForPage(int offset, int size) {
+        if (offset < 1 || size < 1) {
+            return Collections.emptyList();
+        }
+        // TODO,从配置属性中获取应用基础包路径
+        String appPackage = "";
+        List<String> loadedClasses = InstrumentationContext.getLoadedClasses(appPackage);
+        return pageList(offset, size, loadedClasses);
+    }
+
+    @Override
+    @FRPCService
+    public List<String> listClassForPage(int offset, int size, String classShortNameLike) {
+        if (offset < 1 || size < 1) {
+            return Collections.emptyList();
+        }
+        // TODO,从配置属性中获取应用基础包路径
+        String appPackage = "";
+        List<String> loadedClasses = InstrumentationContext.getLoadedClasses(appPackage, classShortNameLike);
+        return pageList(offset, size, loadedClasses);
+    }
+
+    @Override
+    @FRPCService
+    public byte[] getByteCode(String fullClassName) {
+        try {
+            return ByteCodeUtils.getByteCode(fullClassName);
+        } catch (NotFoundException | IOException | CannotCompileException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> List<T> pageList(int offset, int size, List<T> list) {
+        int startIndex = offset - 1;
+        // copy
+        List<T> subList = list.subList(startIndex, startIndex + size);
+        return new ArrayList<>(subList);
+    }
+
+    @Override
+    @FRPCService
+    public void retransformClass(String className) {
+        Class<?> clazz = InstrumentationContext.searchClass(className);
+        if (clazz == null) {
+            throw new RuntimeException(className + " not exist");
+        }
+        try {
+            InstrumentationContext.retransformClasses(clazz);
+        } catch (UnmodifiableClassException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- atom-agent/atom-agent-core/src/main/java/org/mmfmilku/atom/agent/util/ByteCodeUtils.java
+++ atom-agent/atom-agent-core/src/main/java/org/mmfmilku/atom/agent/util/ByteCodeUtils.java
@@ -14,29 +14,12 @@
  *********************************************/
 package org.mmfmilku.atom.agent.util;
 
-import javassist.ClassPool;
-import javassist.CtClass;
-import javassist.LoaderClassPath;
-import org.mmfmilku.atom.agent.compiler.parser.Parser;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.Class;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.JavaAST;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.Method;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.express.Expression;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.express.Identifier;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.statement.CodeBlock;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.statement.Statement;
-import org.mmfmilku.atom.agent.compiler.parser.syntax.statement.VarDefineStatement;
+import javassist.*;
 import org.mmfmilku.atom.agent.context.InstrumentationContext;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * ByteCodeUtils
@@ -51,68 +34,18 @@ public class ByteCodeUtils {
         try (InputStream inputStream = new ByteArrayInputStream(byteCode)) {
             final ClassPool classPool = ClassPool.getDefault();
             return classPool.makeClass(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw e;
         }
     }
 
+    public static byte[] getByteCode(String fullClassName) throws NotFoundException, IOException, CannotCompileException {
+        ClassPool classPool = ClassPool.getDefault();
+        CtClass clazz = classPool.get(fullClassName);
+        return clazz.toBytecode();
+    }
+    
     public static void appendClassPath(ClassLoader classLoader) {
         ClassPool classPool = ClassPool.getDefault();
         classPool.appendClassPath(new LoaderClassPath(classLoader));
-    }
-
-    public static Map<String, Map<String, Method>> toJavassistCode(JavaAST ...javaASTArr) {
-        Map<String, Map<String, Method>> classMap = new HashMap<>();
-        Stream.of(javaASTArr)
-                .map(javaAST -> {
-                    javaAST.useImport();
-                    return javaAST.getClassList();
-                })
-                .flatMap(Collection::stream)
-                .forEach(clazz -> {
-                    Map<String, Method> collect = clazz.getMethods()
-                            .stream()
-                            .peek(ByteCodeUtils::toJavassistCode)
-                            .collect(Collectors.toMap(Method::getMethodName, v -> v));
-                    classMap.put(clazz.getClassFullName(), collect);
-                });
-        return classMap;
-    }
-
-    /**
-     * 方法参数调用替
-     * this -> $0
-     * var1 -> $1
-     * var2 -> $2
-     * ...
-     */
-    public static void toJavassistCode(Method method) {
-        CodeBlock codeBlock = method.getCodeBlock();
-        List<VarDefineStatement> methodParams = method.getMethodParams();
-        if (methodParams == null || methodParams.size() == 0) {
-            return;
-        }
-        Map<String, String> paramMap = new HashMap<>(methodParams.size() + 1);
-        paramMap.put("this", "$0");
-        for (int i = 0; i < methodParams.size(); i++) {
-            VarDefineStatement defineStatement = methodParams.get(i);
-            paramMap.put(defineStatement.getVarName(), "$" + (i + 1));
-        }
-        List<Statement> statements = codeBlock.getStatements();
-        for (Statement statement : statements) {
-            List<Expression> allExpression = statement.getAllExpression();
-            allExpression.stream()
-                    .map(Expression::getBaseExpression)
-                    .flatMap(Collection::stream)
-                    .forEach(expression -> {
-                        if (expression instanceof Identifier) {
-                            Identifier identifier = (Identifier) expression;
-                            String value = identifier.getValue();
-                            identifier.setValue(paramMap.getOrDefault(value, value));
-                        }
-                    });
-        }
     }
 
 }
